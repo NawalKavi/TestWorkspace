20b04001b5d0942b1a2dca18e3a57bd8f7ca4073
Nawal <nawalkavi@gmail.com>
2023-02-28 16:13:34 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-temperatures = [["Band A", 0, 10],
-                ["Band B", 11, 20],
-                ["Band C", 21, 30],
-                ["Band D", 30]]
-
-row = 0
-col = 0
-band = 0
-startTemp = 1
-endTemp = 2
-for row in temperatures:
-    for col in row:
-        if
+nameArray = []
+for nameIndex in range(0,10):
+    name = input("Enter a name: ")
+    nameArray.append(name)
+print(nameArray)
