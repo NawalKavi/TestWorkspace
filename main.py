b4b3e4b4a70ab6d0dd8e3a2eae55d453bfb9745b
Nawal <nawalkavi@gmail.com>
2023-02-28 09:36:04 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,13 @@
-displayGrid = [["B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B"],
-               ["B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B"],
-               ["B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B"],
-               ["B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B", "B"]]
+temperatures = [["Band A", 0, 10],
+                ["Band B", 11, 20],
+                ["Band C", 21, 30],
+                ["Band D", 30]]
 
-length = 0
-charactersLeft = 0
-currentWord = 0
-
-# User input for the message.
-message = input("Please enter a message. \n")
-messageArray = message.split()
-
-# Displaying the grid.
-for loop in displayGrid:
-    string = ""
-    for eachLine in loop:
-        string = string + "   " + eachLine
-    print(string, "\n")
-
-# Blank checker.
-rowBlank = 0
-columnBlank = 0
-while displayGrid[rowBlank][columnBlank] == "B":
-    # Calculating whether the word will fit.
-    row = 0
-    column = 0
-    pos = 0
-    enoughSpace = False
-    wordLength = len(messageArray[currentWord])
-    while charactersLeft < wordLength:
-        while enoughSpace != True:
-            if displayGrid[row][pos] == "B":
-                charactersLeft += 1
-                pos += 1
-                if charactersLeft == wordLength:
-                    enoughSpace = True
-            else:
-                if row < 4:
-                    row += 1
-                    pos = 0
-                    charactersLeft = 0
-                else:
-                    print("Message is too long to be displayed.")
+row = 0
+col = 0
+band = 0
+startTemp = 1
+endTemp = 2
+for row in temperatures:
+    for col in row:
+        if
