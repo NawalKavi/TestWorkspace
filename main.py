6d252ac7b2bd7bae1f2cada5d11384d8bd729275
Nawal <nawalkavi@gmail.com>
2023-03-07 10:50:37 +0000

    Frogger push

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
-nameArray = []
-for nameIndex in range(0,10):
-    name = input("Enter a name: ")
-    nameArray.append(name)
-print(nameArray)
+# Initialising.
+from pygame import *
+init()
+
+# Game window parameters.
+width = 1200
+height = 700
+mainWindow = display.set_mode((width, height))
+display.set_caption("HE CAN'T KEEP GETTING AWAY WITH THIS")
+
+# Game loop.
+oneSecondTick = 0
+gameRunning = True
+while gameRunning == True:
+    for gameEvent in event.get():
+        # Quits game.
+        if gameEvent.type == QUIT:
+            gameRunning = False
+    display.flip()
